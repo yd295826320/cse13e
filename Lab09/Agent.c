93b40eeec742e82eb1fc24e13f4bcdfeec884f69
Duo Yu <dyu33@ucsc.edu>
2021-06-07 14:54:32 -0700

    Replace Agent.c

--- Lab09/Agent.c
+++ Lab09/Agent.c
@@ -16,14 +16,22 @@ static Field opp_field;
 static AgentState currentState;
 //nagotiationDate Hash for yourself and oppo
 static NegotiationData hash,A,B;
+static NegotiationOutcome coinflip;
 static uint8_t turnnum;
 //field oled turn
 static FieldOledTurn whoseturn;
 static GuessData guessData;
-static uint16_t Hash_a;
+static GuessData opp_guess;
+//static uint16_t Hash_a;
 static SquareStatus previous;
+static uint8_t battle_result;
 
+#define defeat 0
+#define victory 1
 
+void NegotiationScreen(void);
+void  battle(void);
+void  screen(void);
 /**
  * The Init() function for an Agent sets up everything necessary for an agent before the game
  * starts.  At a minimum, this requires:
@@ -57,14 +65,20 @@ void AgentInit(void){
  */
 Message AgentRun(BB_Event event){
     //state machine
+    BOARD_Init();
+    OledInit();
+    
     Message Messageout = {MESSAGE_NONE, 0, 0, 0};
     if (event.type == BB_EVENT_RESET_BUTTON){
         AgentInit();
-        //NegotiationScreen
+        NegotiationScreen();
         return Messageout;
     }
-    if (event.type = BB_EVENT_ERROR){
+    if (event.type == BB_EVENT_ERROR){
         //errorScreen
+        OledDrawString("ERRORS!\n");
+        OledUpdate();
+        
         Messageout.type = MESSAGE_ERROR;
         Messageout.param0 = event.param0;
         Messageout.param1 = event.param1;
@@ -76,18 +90,20 @@ Message AgentRun(BB_Event event){
     switch(currentState){
         case AGENT_STATE_START:
             if (event.type == BB_EVENT_START_BUTTON){
+                NegotiationScreen();
                 currentState = AGENT_STATE_CHALLENGING;
                 A = rand();
-                Hash_a = NegotiationHash(A);
+                hash = NegotiationHash(A);
                 Messageout.type = MESSAGE_CHA;
-                Messageout.param0 = Hash_a;
+                Messageout.param0 = hash;
                 FieldInit(&own_field,&opp_field);
                 FieldAIPlaceAllBoats(&own_field);
                 
             }
-            else if(event.type == BB_EVENT_CHA_RECEIVED){
+            if(event.type == BB_EVENT_CHA_RECEIVED){
+                NegotiationScreen();
                 currentState = AGENT_STATE_ACCEPTING;
-                Hash_a = event.param0;
+                hash = event.param0;
                 B = rand();
                 Messageout.type = MESSAGE_ACC;
                 Messageout.param0 = B;
@@ -99,44 +115,54 @@ Message AgentRun(BB_Event event){
             break;
         case AGENT_STATE_CHALLENGING:
             if (event.type == BB_EVENT_ACC_RECEIVED){
-                Messageout.type = MESSAGE_REV;
-                B = NegotiationHash(event.param0);
-                Messageout.param0 = A;
-                Messageout.param1 = 0;
-                Messageout.param2 = 0;
-                // updata screen
-                if (NegotiateCoinFlip(A, B) == HEADS){                                   //question
+                
+                coinflip = NegotiateCoinFlip(A, B);
+                if (coinflip == HEADS){                                   //question
                     currentState = AGENT_STATE_WAITING_TO_SEND;
+                    whoseturn = FIELD_OLED_TURN_MINE;
+                    screen();
                 }
                 else{
                     currentState = AGENT_STATE_DEFENDING;
+                    whoseturn = FIELD_OLED_TURN_THEIRS;
                 }
-               
+                Messageout.type = MESSAGE_REV;
+                B = event.param0;
+                Messageout.param0 = A;
+                return Messageout;
             }
             break;
         case AGENT_STATE_ACCEPTING:
             if (event.type == BB_EVENT_REV_RECEIVED){
                 A = event.param0;
-                uint8_t checkcheat = NegotiationVerify(A,Hash_a);
+                screen();
+                uint8_t checkcheat = NegotiationVerify(A,hash);
                 if (checkcheat == FALSE){
                     Messageout.type = -1;//error Message
                     Messageout.param0 = A;
-                    Messageout.param1 = Hash_a;
+                    Messageout.param1 = hash;
                     currentState = AGENT_STATE_END_SCREEN;
                 }
                 else{
-                    if (NegotiateCoinFlip(A,B) == TAILS){
+                    coinflip = NegotiateCoinFlip(A,B);
+                    if (coinflip == TAILS){
+                        hash = NegotiationHash(A);
+                        whoseturn = FIELD_OLED_TURN_MINE;
+                        battle();
                         guessData = FieldAIDecideGuess(&opp_field);
                         Messageout.type = MESSAGE_SHO;
                         Messageout.param0 = guessData.row; 
                         Messageout.param1 = guessData.col;
                         Messageout.param2 = 0;
                         currentState = AGENT_STATE_ATTACKING;
-                        break;
+                        
                     }
                     else{
+                        whoseturn = FIELD_OLED_TURN_THEIRS;
+                        battle();
                         currentState = AGENT_STATE_DEFENDING;
                     }
+                    return Messageout;
                 }
             }
             break;
@@ -147,51 +173,71 @@ Message AgentRun(BB_Event event){
                 Messageout.type = MESSAGE_SHO;
                 Messageout.param0 = AI_guess.row;
                 Messageout.param1 = AI_guess.col;
-                Messageout.param2 = 0;
                 currentState = AGENT_STATE_ATTACKING;
+                return Messageout;
             }
             break;
         case AGENT_STATE_ATTACKING:
             if (event.type == BB_EVENT_RES_RECEIVED){
                 //0 for row 1 for col 2 for guessdata
                 FieldUpdateKnowledge(&opp_field, &guessData); 
-                //guessData.row = event.param0;
-                //guessData.col = event.param1;
-                //guessData.result = event.param2;
+                guessData.row = event.param0;
+                guessData.col = event.param1;
+                guessData.result = event.param2;
+                Messageout.param0 = guessData.row;
+                Messageout.param1 = guessData.col;
+                battle();
                 
                 if (FieldGetBoatStates(&opp_field) == 0b00000000){
-                    //victory
+                   battle_result == victory;
                    currentState = AGENT_STATE_END_SCREEN; 
                 }
                 else{
+                    whoseturn = FIELD_OLED_TURN_THEIRS;
+                    battle();
                     currentState = AGENT_STATE_DEFENDING; 
+                    return Messageout;
                 }
             }
             break;
         case AGENT_STATE_DEFENDING:
             if (event.type == BB_EVENT_SHO_RECEIVED){
-                GuessData AI_guess2 = FieldAIDecideGuess(&own_field); 
-                previous = FieldRegisterEnemyAttack(&own_field, &AI_guess2);
-                FieldUpdateKnowledge(&own_field, &AI_guess2);
-                //guessData.row = event.param0;
-                //guessData.col = event.param1;
-                //FieldRegisterEnemyAttack(&own_field, &guessData);
-                //Messageout.type = MESSAGE_RES;
+                opp_guess.row = event.param0;
+                opp_guess.col = event.param1;
+                FieldRegisterEnemyAttack(&own_field, &opp_guess);
+                Messageout.param0 = opp_guess.row;
+                Messageout.param1 = opp_guess.col;
+                Messageout.param2 = opp_guess.result;
+                Messageout.type = MESSAGE_RES;
+                battle();
                 if (FieldGetBoatStates(&own_field) == 0b00000000){
-                    //defeat
+                   battle_result == defeat;
                    currentState = AGENT_STATE_END_SCREEN; 
+                   return Messageout;
                 }
                 else{
-                    Messageout.type = MESSAGE_RES;
-                    Messageout.param0 = AI_guess2.row;
-                    Messageout.param1 = AI_guess2.col;
-                    Messageout.param2 = AI_guess2.result;
-                    currentState = AGENT_STATE_WAITING_TO_SEND; 
+                    currentState = AGENT_STATE_WAITING_TO_SEND;
+                    whoseturn = FIELD_OLED_TURN_THEIRS;
+                    battle();
+                    return Messageout;
                 }
             }
             break;
         case AGENT_STATE_END_SCREEN:
-            
+            OledClear(OLED_COLOR_BLACK);
+            char display2[100] = "";
+            if (battle_result == defeat){
+                sprintf(display2,"You LOST!");
+            }
+            if (battle_result == victory){
+                sprintf(display2,"You WIN!");
+            }
+            OledDrawString(display2);
+            OledUpdate();
+            OledClear(OLED_COLOR_BLACK);
+            sprintf(display2,"Thanks for playing!");
+            OledDrawString(display2);
+            OledUpdate();
             break;
     }
     
@@ -216,6 +262,23 @@ void AgentSetState(AgentState newState){
     currentState = newState;
 }
 
+void  screen(void){
+    char display[100] = "";
+    OledClear(OLED_COLOR_BLACK);
+    
+    sprintf(display, "CHALLENGING & ACCEPTING\n");
+    OledDrawString(display);
+    OledUpdate();
+}
 
+void  battle(void){
+    FieldOledDrawScreen(&own_field, &opp_field, whoseturn, turnnum);
+    OledUpdate();
+}
+
+void NegotiationScreen(void){
+    OledDrawString("This is BattleBoats!\nPress BTN4 to\nchallenge, or wait\nfor opponent.\n");
+    OledUpdate();
+}
 //#endif // AGENT_H
 
